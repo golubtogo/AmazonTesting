f342b1a6f2c0645e3687ed067ad86457675e18c5
Nata <golub.togo@gmail.com>
2024-02-17 21:27:11 +0100

    add 5 simple tests

---
+++ src/test/java/simple/test/removeExtraSpaces.java
@@ -0,0 +1,21 @@
+//Write Java code to get rid of multiple spaces from a string.
+package simple.test;
+
+import java.util.StringTokenizer;
+
+public class removeExtraSpaces {
+
+    public static void main(String args[]) {
+
+        String input = "Try    to    remove   extra   spaces.";
+        StringTokenizer substr = new StringTokenizer(input, " ");
+        StringBuffer sb = new StringBuffer();
+
+        while (substr.hasMoreElements()) {
+            sb.append(substr.nextElement()).append(" ");
+        }
+
+        System.out.println("Actual string: " + input);
+        System.out.println("Processed string: " + sb.toString().trim());
+    }
+}
